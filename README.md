2e8b7c0f2f5a4598fb53a48989afeb86b690b0c6
pandov <ipandov17@gmail.com>
2021-12-09 23:43:31 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# ApplStat_HW
+# Applied Statistics / Home Work
+
+## Reproducibility
+
+|        |               |
+| -----  | ------------- |
+| OS     | Windows/Linux |
+| Python | 3.7.7         |
+| pip    | 21.1.2        |
+
+&nbsp;
+### Dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+### Run:
+```bash
+python main.py
+```
+
+## Plots
+
+### a. Joint Histogram
+![a. Joint Histogram](img/a.png)
+
+### b. Box Plots
+![b. Box Plots](img/b.png)
+
+### c. Empirical Distribution
+![c. Empirical Distribution](img/c.png)
--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-# %%
 import os.path
 import pandas as pd
 import numpy as np
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-# from sklearn.preprocessing import *
+from statsmodels.distributions.empirical_distribution import ECDF
+
 
 filename = 'Protein lengths.xlsx'
-
 bacteria = pd.read_excel(filename, sheet_name='Бактерии')
 eukaryotes = pd.read_excel(filename, sheet_name='Эукариоты')
 
-# %%
 def processing(df, series):
-    df = df.rename(columns={
-        'Name': 'Name.0',
-        'Length': 'Length.0',
-    })
+    df = df.rename(columns={'Name': 'Name.0', 'Length': 'Length.0'})
     frames = dict()
     for column in df.columns.tolist():
         if 'Name' not in column: continue
@@ -26,7 +21,7 @@ def processing(df, series):
         frame = pd.DataFrame({
             'Name': split[0],
             'Mnemonics': split[1],
-            'Length': df[f'Length.{n}']
+            'Length': df[f'Length.{n}'],
         }).dropna(axis=0)
         index = frame.pop('Mnemonics').unique()
         assert len(index) == 1
@@ -40,60 +35,37 @@ eukaryotes_mnemonics = pd.read_csv('csv/eukaryotes.csv', index_col='Мнемон
 bacteria_ = processing(bacteria, bacteria_mnemonics)
 eukaryotes_ = processing(eukaryotes, eukaryotes_mnemonics)
 
-# %%
-# for key1, key2 in zip(bacteria_, eukaryotes_):
-#     print(key1, len(bacteria_[key1]), key2, len(eukaryotes_[key2]))
-
-# %%
-
-dist = [
-    bacteria_['ECOLI'].copy(),
-    eukaryotes_['YEAST'].copy(),
-]
-legend = [
-    d.index[0][0]
-    for d in dist
-]
-log_dist = [
-    np.log10(d.values)
-    for d in dist
-]
+dist = [bacteria_['ECOLI'].copy(), eukaryotes_['YEAST'].copy()]
+log_dist = [np.log10(d.values) for d in dist]
+legend = [d.index[0][0] for d in dist]
 
 def figure():
     fig = plt.figure(figsize=(12, 9))
     return fig, fig.gca()
 
+
 fig, ax = figure()
 ax.hist(log_dist, bins=50, alpha=0.75)
 ax.legend(legend, loc='upper left')
-ax.set_title('a. Joint histogram')
+ax.set_title('a. Joint Histogram')
 ax.set_xlabel('Protein Length, log10')
-ax.set_ylabel('Probability density')
-# plt.show()
+ax.set_ylabel('Probability Density')
 fig.savefig('img/a.png')
 
-# %%
+
 fig, ax = figure()
 ax.boxplot(log_dist, widths=0.4)
 ax.set_xticklabels(legend)
-ax.set_title('b. Box plots')
+ax.set_title('b. Box Plots')
 ax.set_ylabel('Protein Length, log10')
-# plt.show()
 fig.savefig('img/b.png')
 
-# %%
-from statsmodels.distributions.empirical_distribution import ECDF
-ecdf = [
-    ECDF(d.squeeze())
-    for d in log_dist
-]
+
+ecdf = [ECDF(d.squeeze()) for d in log_dist]
 fig, ax = figure()
 ax.plot(ecdf[0].x, ecdf[0].y, alpha=0.75)
 ax.plot(ecdf[1].x, ecdf[1].y, alpha=0.75)
 ax.legend(legend, loc='upper left')
-ax.set_title('c. Empirical distribution')
+ax.set_title('c. Empirical Distribution')
 ax.set_xlabel('Protein Length, log10')
-# plt.show()
 fig.savefig('img/c.png')
-
-# %%
